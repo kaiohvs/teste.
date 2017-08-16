899f8e9c1e53504a788fadd4ebd4a08566f170c3
kaio.hvsantos <kaio.hvsantos@CAS511765W0774.senacsp.edu.br>
2017-08-15 21:27:43 -0300

    java.teste01

---
+++ aula02/src/main/java/br/senactads3/pi3b/aula02/principal.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senactads3.pi3b.aula02;
+
+/**
+ *
+ * @author kaio.hvsantos
+ */
+public class principal {
+    public static void main(String[] args) {
+        System.out.println("hello word PI3B");
+    }
+}
